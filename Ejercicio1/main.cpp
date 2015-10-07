04a13f8ed69c6371562807a865e4664934b86d28
rubengf <ruben.rgf@gmail.com>
2015-10-07 09:19:48 +0200

    Initial Commit

---
+++ Ejercicio1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ejercicio1
+//
+//  Created by Rubén Gómez on 7/10/15.
+//  Copyright © 2015 Rubén Gómez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
