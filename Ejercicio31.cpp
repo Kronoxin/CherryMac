dc3f89038d2e3f6a0e5038da1e719357f60d2719
rubengf <ruben.rgf@gmail.com>
2016-01-21 17:05:34 +0100

    Empezando el 31 (entrada)

---
+++ Ejercicio31.cpp
@@ -0,0 +1,65 @@
+//
+//  Ejercicio31.cpp
+//  Ejercicio1
+//
+
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include "Matriz.h"
+
+bool resuelveCaso()
+{
+    
+    //  monedas(i,j) =
+    //
+    //  monedas(i,0) = 0
+    //  monedas(0,j) = inf.
+    //
+    //  monedas(i,j) =  monedas(i-1,j)      xi > j.
+    //  monedas(i,j) =  1                   xi = j.
+    //  monedas(i,j) =  min{ monedas(i-1), monedas(i-1,j-xi)+1 }
+    //
+    const int infinidad= 1000001;
+    int nMonedas;
+    
+    std::cin >> nMonedas;
+    
+    
+    
+    if(!std::cin)               // si no tenemos entrada, terminamos
+        return false;
+    
+    Matriz<int> monedas(nMonedas,nMonedas,infinidad);
+    
+    for (int i = 0 ; i< nMonedas ; i++)
+    {
+        for (int j = i; j < nMonedas; j++)
+        {
+            
+            if( i == j)
+            {
+                monedas[i][j] = 0;
+            }
+            else{
+                int c;
+                std::cin >> c;
+                monedas[i][j] = c;
+            }
+            
+            
+        }
+    }
+    
+    
+    return true;
+    
+}
+
+
+int main()
+{
+    while(resuelveCaso());
+    return 0;
+}
